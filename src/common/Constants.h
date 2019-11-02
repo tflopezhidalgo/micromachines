feb88b26062bcd8c29aeb05b925a5f83ebb8656f
Leonardo <leobellaera@gmail.com>
2019-11-02 03:47:07 -0300

    agrego constantes de los tracks shapes al Constant.h

--- src/common/Constants.h
+++ src/common/Constants.h
@@ -9,10 +9,17 @@
 //protocol client-sv
 #define SERIALIZING_RESCAILING 1000
 #define CREATE_MATCH_MSG "create"
-#define JOIN_MATCH_MSG "join"
 #define VALID 0
 #define MATCH_HAS_STARTED 1
 #define MATCH_EQUAL_NAMED 2
 #define CLIENT_EQUAL_NAMED 3
+
+//track shapes
+#define HORIZONTAL_TRACK 1
+#define VERTICAL_TRACK 2
+#define FIRST_QUADRANT_TRACK 3
+#define SECOND_QUADRANT_TRACK 4
+#define THIRD_QUADRANT_TRACK 5
+#define FOURTH_QUADRANT_TRACK 4
 
 #endif //MICROMACHINES_CONSTANTS_H
