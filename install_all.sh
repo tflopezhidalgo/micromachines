e029a309fbb64f06f5153d92100199524f6a02ef
Elianagam <elianagam2@gmail.com>
2019-11-23 18:26:58 -0300

    parches

--- install_all.sh
+++ install_all.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-sudo apt-get update && sudo apt-get install -y  libegl1-mesa-dev libgles2-mesa-dev libsdl2-dev libsdl2-image-dev nlohmann-json-dev lua5.3 liblua5.3-dev qt5-default libsdl2-mixer-dev libavutil-dev libswresample-dev libavformat-dev libavcodec-dev libswscale-dev libsdl2-ttf-dev
+sudo apt-get update && sudo apt-get install -y  libegl1-mesa-dev libgles2-mesa-dev libsdl2-dev libsdl2-image-dev nlohmann-json-dev lua5.3 liblua5.3-dev qt5-default libsdl2-mixer-dev libavutil-dev libswresample-dev libavformat-dev libavcodec-dev libswscale-dev libsdl2-ttf-dev mesa-common-dev libglu1-mesa-dev freeglut3-dev
 
 mkdir build
 cd build
