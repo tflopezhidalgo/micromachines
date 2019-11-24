fa634c32f0b299970435e560577faabf8b06f2e3
Leonardo <leobellaera@gmail.com>
2019-11-24 18:45:27 -0300

    Agrega tiles

--- src/client/TileMap.h
+++ src/client/TileMap.h
@@ -9,7 +9,7 @@
 #include <iostream>
 #include <json.hpp>
 
-#define TILE_0 "../media/tiles/Grass_Tile.png"
+#define TILE_0 "../media/tiles/00_Road.png"
 #define TILE_1 "../media/tiles/01_Road.png"
 #define TILE_2 "../media/tiles/02_Road.png"
 #define TILE_3 "../media/tiles/03_Road.png"
@@ -18,6 +18,12 @@
 #define TILE_6 "../media/tiles/06_Road.png"
 #define TILE_7 "../media/tiles/07_Road.png"
 #define TILE_8 "../media/tiles/08_Road.png"
+#define TILE_9 "../media/tiles/09_Road.png"
+#define TILE_10 "../media/tiles/10_Road.png"
+#define TILE_11 "../media/tiles/11_Road.png"
+#define TILE_12 "../media/tiles/12_Road.png"
+#define TILE_13 "../media/tiles/13_Road.png"
+#define TILE_14 "../media/tiles/14_Road.png"
 
 class TileMap: public Entity{
     private:
